a56939e1dc27e660997653905d25af9109045f46
Daniel Wise <dwise03@gmail.com>
2026-08-18 19:28:01 -0400

    fix: Simplify next.config.js to fix runtime errors

--- apps/studio/next.config.js
+++ apps/studio/next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   output: 'standalone',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
