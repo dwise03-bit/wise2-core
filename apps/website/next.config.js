2d20891b98bcda53bf6ab6b01989aab9bc0563ed
Daniel Wise <dwise03@gmail.com>
2026-08-18 23:05:38 -0400

    fix: disable trailing slash redirects in website app next.config

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next).NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  trailingSlash: false,
   reactStrictMode: true,
   productionBrowserSourceMaps: false,
   generateBuildId: async () => {
